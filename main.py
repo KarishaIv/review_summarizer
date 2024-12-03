d30cfe7078b5c9becd965137355e816d0f25a828
coranmiel <145856292+coranmiel@users.noreply.github.com>
2024-12-03 13:07:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
-from messages import WELCOME_MESSAGE
+from messages import WELCOME_MESSAGE, EATING_MESSAGE, STICKERS
 import random
-from messages import STICKERS
 
 load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -19,15 +18,25 @@ async def send_message(chat_id: int, text: str, reply_markup=None):
 async def send_message_with_menu(chat_id: int, text: str):
     await send_message(chat_id, text, reply_markup=keyboard_reply)
 
-async def handle_kushat_response(chat_id: int):
-    await send_message_with_menu(chat_id, "ааааа карина я хз какой функционал \n и что ваще бот должен спрашивать")
-
 async def send_sticker(chat_id: int, sticker_id: str):
     await bot.send_sticker(chat_id, sticker_id)
 
 keyboard_inline = InlineKeyboardMarkup().add(InlineKeyboardButton(text="я хочу кушать...", callback_data="first_button"))
 button_menu = KeyboardButton("в меню")
 keyboard_reply = ReplyKeyboardMarkup(resize_keyboard=True).add(button_menu)
+keyboard_eating = InlineKeyboardMarkup().add(
+    InlineKeyboardButton(text="один", callback_data="button_one"),
+    InlineKeyboardButton(text="два", callback_data="button_two")
+)
+
+@dp.callback_query_handler(text=["first_button"])
+async def check_button(call: types.CallbackQuery):
+    await send_message(call.message.chat.id, EATING_MESSAGE, reply_markup=keyboard_eating)
+    await call.answer()
+
+@dp.message_handler(commands=['kushat'])
+async def help_command(message: types.Message):
+    await send_message(message.chat.id, EATING_MESSAGE, reply_markup=keyboard_eating)
 
 '''     старт      '''
 @dp.message_handler(commands=['start'])
@@ -42,16 +51,6 @@ async def menu_button_click(message: types.Message):
 @dp.message_handler(commands=['menu'])
 async def menu_command(message: types.Message):
     await send_message_with_menu(message.chat.id, WELCOME_MESSAGE)
-
-'''     обработка нажатий на кнопки '''
-@dp.callback_query_handler(text=["first_button"])
-async def check_button(call: types.CallbackQuery):
-    await handle_kushat_response(call.message.chat.id)
-    await call.answer()
-
-@dp.message_handler(commands=['kushat'])
-async def help_command(message: types.Message):
-    await handle_kushat_response(message.chat.id)
 
 '''     обработка команды /fact для отправки стикера '''
 @dp.message_handler(commands=['fact'])
@@ -69,6 +68,7 @@ async def else_msg(message: types.Message):
     else:
         response = "нет хуй" if message.text == 'пенис' else "пенис"
         await send_message_with_menu(message.chat.id, response)
+
 
 if __name__ == '__main__':
     try:
