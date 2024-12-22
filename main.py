809c85bce6da2d884aac3177143d788534e119c3
coranmiel <145856292+coranmiel@users.noreply.github.com>
2024-12-23 01:21:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from aiogram.utils import executor
+from chosen_recipe import dp as dp_chosen
+from random_recipe import dp as dp_random
+
+dp = dp_chosen
+
 if __name__ == '__main__':
     try:
         executor.start_polling(dp, skip_updates=True)
